8cd9326d0272600fac16533b36461adbf0499af9
unknown <maxammadjonovibroximjon24@gmail.com>
2022-09-25 18:24:52 +0500

    added files

---
+++ main/bin/class.dart
@@ -0,0 +1,12 @@
+class User {
+  int _id = 12;
+  String _username = '';
+  User(int id, String username) {
+    _id = id;
+    _username = username;
+  }
+  @override
+  String toString() {
+    return 'User(id: $_id, name: "$_username")';
+  }
+}
---
+++ main/bin/main.dart
@@ -0,0 +1,8 @@
+import 'dart:ffi';
+
+import 'class.dart';
+
+void main(List<String> args) {
+  User user = User(12, 'Ibroximjon');
+  print(user);
+}
